e3af7da507314a5fae235fb9a871a94be988bf7e
“Liamvdm2” <L1am20041711@gmail.com>
2024-01-16 11:12:28 +0100

    now able to get users from the db

--- Larapress/larapress/routes/api.php
+++ Larapress/larapress/routes/api.php
@@ -2,18 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+
+Route::get('/users', function () {
+    $results = DB::table('Users')
+        ->select([
+            'user_id',
+            'name',
+            'surname',
+            'email',
+            'password',
+            'birthday',
+        ])
+        ->get();
+ 
+    return response()->json($results);
 });
+
