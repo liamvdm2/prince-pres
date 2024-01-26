41f9cdac772da0e27f81391beed372dbf6433b63
“Liamvdm2” <L1am20041711@gmail.com>
2024-01-26 21:45:51 +0100

    news created. will work when bootstrap works

--- prince/src/app/shared/buzz.service.ts
+++ prince/src/app/shared/buzz.service.ts
@@ -7,13 +7,13 @@ import { Injectable } from '@angular/core';
 export class BuzzService {
 
 
-    async buzz(title: any, description: any, author: any) {
-	
+	async buzz(title: any, description: any, author: any) {
+
 
 		const Buzz = {
-            news_title: title,
-            news_description: description,
-            news_author: author
+			news_title: title,
+			news_description: description,
+			news_author: author
 		};
 		const result = await fetch('http://127.0.0.1:8000/api/buzz', {
 			method: 'POST',
@@ -23,5 +23,19 @@ export class BuzzService {
 			body: JSON.stringify(Buzz)
 		});
 		return result.json();
+	}
+
+	async getBuzz() {
+		try {
+			const response = await fetch('http://127.0.0.1:8000/api/buzz');
+			if (!response.ok) {
+				throw new Error('Failed to fetch news');
+			}
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error('Error fetching users:', error);
+			throw error;
+		}
 	}
 }
