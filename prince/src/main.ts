74e9fd6d491e440e2a9d13f133e3818dbc353474
UgurUgurUgur <ugurkankaraca03@gmail.com>
2024-01-28 21:00:26 +0100

    Smoll changes - removed swiper

--- prince/src/main.ts
+++ prince/src/main.ts
@@ -3,13 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from "@angular/common/http";
 import 'zone.js';
-import { register as registerSwiperElements } from 'swiper/element/bundle';
-
-// Register Swiper custom elements. We do this
-// before bootstrapping the Angular application
-// so that they're available before any part of
-// our application tries rendering them.
-registerSwiperElements();
 
 bootstrapApplication(AppComponent, {
  ...appConfig,
