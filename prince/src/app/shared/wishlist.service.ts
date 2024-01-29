9ece7eae958906d171246ff57a3c6b77778d5781
ElineBoffinPXL <eline.boffin@student.pxl.be>
2024-01-29 07:36:42 +0100

    fixed wishlist button style

--- prince/src/app/shared/wishlist.service.ts
+++ prince/src/app/shared/wishlist.service.ts
@@ -8,7 +8,7 @@ import { UserService } from './user.service';
   providedIn: 'root'
 })
 export class WishlistService {
-  
+
   private apiUrl = 'http://127.0.0.1:8000/api';
   private userObject = {
     user_id: undefined
@@ -16,30 +16,22 @@ export class WishlistService {
 
 
 
-  constructor(private http: HttpClient, private userService: UserService) { 
+  constructor(private http: HttpClient, private userService: UserService) {
     userService.currentUser.subscribe(user => {
       this.userObject = user;
-      
+
     });
-    console.log('test', this.userObject);
- }
- 
-
-addToWishlist(productId: number) {
-  const url = `${this.apiUrl}/wishlist/${this.userObject.user_id}/${productId}`;
-  return this.http.post(url, {});
-}
-
-getWishlist() {
-  const url = `${this.apiUrl}/wishlist/${this.userObject.user_id}`;
-  return this.http.get(url);
- }
-/*public async addToWishlist(userId: string, productId: string): Promise<any> {
-  try {
-      const response = await this.http.post('http://127.0.0.1:8000/api/wishlist/%7Busername%7D', { userId, productId }).toPromise();
-      return response;
-  } catch (error) {
-      console.error(error);
   }
-}*/
+
+
+  addToWishlist(productId: number) {
+    const url = `${this.apiUrl}/wishlist/${this.userObject.user_id}/${productId}`;
+    return this.http.post(url, {});
+  }
+
+  getWishlist() {
+    const url = `${this.apiUrl}/wishlist/${this.userObject.user_id}`;
+    return this.http.get(url);
+  }
+
 }
